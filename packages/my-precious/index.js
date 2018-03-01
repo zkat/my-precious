7a5953e2c3ced731a4813ac8c0acf4ed9dd6cc0c
Kat Marchán <kzm@sykosomatic.org>
2018-02-28 16:02:30 -0800

    feat(reset): add support for resetting to pre-archive state

--- packages/my-precious/index.js
+++ packages/my-precious/index.js
@@ -15,29 +15,34 @@ function cliMain () {
   return fromNpm(process.argv)
   .then(config => {
     npmlog.level = config.get('loglevel')
-    return new MyPrecious({
+    const precious = new MyPrecious({
       config,
       log: npmlog
     })
-  })
-  .then(precious => precious.run())
-  .then(details => {
-    const clauses = []
-    if (!details.pkgCount && !details.removed) {
-      clauses.push('done')
+    if (process.argv.find(x => x === 'reset')) {
+      return precious.unarchive()
+      .then(() => console.error('archive data removed'))
+    } else {
+      return precious.archive()
+      .then(details => {
+        const clauses = []
+        if (!details.pkgCount && !details.removed) {
+          clauses.push('done')
+        }
+        if (details.pkgCount) {
+          clauses.push(`archived ${details.pkgCount} package${
+            details.pkgCount === 1 ? '' : 's'
+          }`)
+        }
+        if (details.removed) {
+          clauses.push(`cleaned up ${details.pkgCount} archive${
+            details.removed === 1 ? '' : 's'
+          }`)
+        }
+        const time = details.runTime / 1000
+        console.error(`${clauses.join(' and ')} in ${time}s`)
+      })
     }
-    if (details.pkgCount) {
-      clauses.push(`archived ${details.pkgCount} package${
-        details.pkgCount === 1 ? '' : 's'
-      }`)
-    }
-    if (details.removed) {
-      clauses.push(`cleaned up ${details.pkgCount} archive${
-        details.removed === 1 ? '' : 's'
-      }`)
-    }
-    const time = details.runTime / 1000
-    console.error(`${clauses.join(' and ')} in ${time}s`)
   })
   .catch(err => {
     npmlog.error('', err.message)
