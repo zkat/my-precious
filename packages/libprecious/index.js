6778359ad88e69c577948e7920e295087c9d1667
Kat Marchán <kzm@zkat.tech>
2018-07-05 14:37:27 -0700

    meta: standard fix

--- packages/libprecious/index.js
+++ packages/libprecious/index.js
@@ -43,76 +43,76 @@ class MyPrecious {
   timedStage (name) {
     const start = Date.now()
     return BB.resolve(this[name].apply(this, [].slice.call(arguments, 1)))
-    .tap(() => {
-      this.timings[name] = Date.now() - start
-      this.log.info(name, `Done in ${this.timings[name] / 1000}s`)
-    })
+      .tap(() => {
+        this.timings[name] = Date.now() - start
+        this.log.info(name, `Done in ${this.timings[name] / 1000}s`)
+      })
   }
 
   run () { return this.archive() }
   archive () {
     return this.timedStage('prepare')
-    .then(() => this.timedStage('findExisting'))
-    .then(() => this.timedStage('saveTarballs', this.tree))
-    .then(() => this.timedStage('updateLockfile', this.tree))
-    .then(() => this.timedStage('cleanupArchives'))
-    .then(() => this.timedStage('teardown'))
-    .then(() => { this.runTime = Date.now() - this.startTime })
-    .catch(err => { this.timedStage('teardown'); throw err })
-    .then(() => this)
+      .then(() => this.timedStage('findExisting'))
+      .then(() => this.timedStage('saveTarballs', this.tree))
+      .then(() => this.timedStage('updateLockfile', this.tree))
+      .then(() => this.timedStage('cleanupArchives'))
+      .then(() => this.timedStage('teardown'))
+      .then(() => { this.runTime = Date.now() - this.startTime })
+      .catch(err => { this.timedStage('teardown'); throw err })
+      .then(() => this)
   }
 
   unarchive () {
     return this.timedStage('prepare')
-    .then(() => this.timedStage('restoreDependencies', this.tree))
-    .then(() => this.timedStage('updateLockfile', this.tree))
-    .then(() => this.timedStage('removeTarballs'))
-    .then(() => this.timedStage('removeModules'))
-    .then(() => {
-      this.runTime = Date.now() - this.startTime
-      this.log.info(
-        'run-time',
-        `total run time: ${this.runTime / 1000}s`
-      )
-    })
-    .catch(err => { this.timedStage('teardown'); throw err })
-    .then(() => this)
+      .then(() => this.timedStage('restoreDependencies', this.tree))
+      .then(() => this.timedStage('updateLockfile', this.tree))
+      .then(() => this.timedStage('removeTarballs'))
+      .then(() => this.timedStage('removeModules'))
+      .then(() => {
+        this.runTime = Date.now() - this.startTime
+        this.log.info(
+          'run-time',
+          `total run time: ${this.runTime / 1000}s`
+        )
+      })
+      .catch(err => { this.timedStage('teardown'); throw err })
+      .then(() => this)
   }
 
   prepare () {
     this.log.info('prepare', 'initializing installer')
     return (
       this.config.get('prefix') && this.config.get('global')
-      ? BB.resolve(this.config.get('prefix'))
-      // There's some Special™ logic around the `--prefix` config when it
-      // comes from a config file or env vs when it comes from the CLI
-      : process.argv.some(arg => arg.match(/^\s*--prefix\s*/i))
-      ? this.config.get('prefix')
-      : getPrefix(process.cwd())
+        ? BB.resolve(this.config.get('prefix'))
+        // There's some Special™ logic around the `--prefix` config when it
+        // comes from a config file or env vs when it comes from the CLI
+        : process.argv.some(arg => arg.match(/^\s*--prefix\s*/i))
+          ? this.config.get('prefix')
+          : getPrefix(process.cwd())
     )
-    .then(prefix => {
-      this.prefix = prefix
-      this.archiveDir = path.join(prefix, 'archived-packages')
-      this.log.verbose('prepare', 'package prefix: ' + prefix)
-      return BB.join(
-        readJson(prefix, 'package.json'),
-        readJson(prefix, 'package-lock.json', true),
-        readJson(prefix, 'npm-shrinkwrap.json', true),
-        (pkg, lock, shrink) => {
-          pkg._shrinkwrap = shrink || lock
-          this.pkg = pkg
-          this.lockName = shrink ? 'npm-shrinkwrap.json' : 'package-lock.json'
-        }
-      )
-    })
-    .then(() => statAsync(
-      path.join(this.prefix, 'node_modules')
-    ).catch(err => { if (err.code !== 'ENOENT') { throw err } }))
-    .then(stat => this.checkLock())
-    .then(() => {
-      this.tree = buildLogicalTree(this.pkg, this.pkg._shrinkwrap)
-      this.log.silly('tree', this.tree)
-    })
+      .then(prefix => {
+        this.prefix = prefix
+        this.archiveDir = path.join(prefix, 'archived-packages')
+        this.log.verbose('prepare', 'package prefix: ' + prefix)
+        return BB.join(
+          readJson(prefix, 'package.json'),
+          readJson(prefix, 'package-lock.json', true),
+          readJson(prefix, 'npm-shrinkwrap.json', true),
+          (pkg, lock, shrink) => {
+            pkg._shrinkwrap = shrink || lock
+            this.pkg = pkg
+            this.lockName = shrink ? 'npm-shrinkwrap.json' : 'package-lock.json'
+          }
+        )
+      })
+      .then(() => statAsync(
+        path.join(this.prefix, 'node_modules')
+      ).catch(err => { if (err.code !== 'ENOENT') { throw err } }))
+      .then(stat => this.checkLock())
+      .then(() => {
+        this.tree = buildLogicalTree(this.pkg, this.pkg._shrinkwrap)
+        this.log.silly('tree', this.tree)
+      })
   }
 
   teardown () {
@@ -144,23 +144,23 @@ class MyPrecious {
   findExisting () {
     this.log.info('findExisting', 'checking for existing archived packages')
     return readdirAsync(this.archiveDir)
-    .catch(err => {
-      if (err.code === 'ENOENT') { return [] }
-      throw err
-    })
-    .then(existing => {
-      return BB.all(
-        existing.filter(f => f.match(/^@/))
-        .map(f => {
-          return readdirAsync(path.join(this.archiveDir, f))
-          .then(subfiles => subfiles.map(subf => `${f}/${subf}`))
-        })
-      )
-      .then(scoped => scoped.reduce((acc, scoped) => {
-        return acc.concat(scoped)
-      }, existing.filter(f => !f.match(/^@/))))
-    })
-    .then(allExisting => { this.existingArchives = new Set(allExisting) })
+      .catch(err => {
+        if (err.code === 'ENOENT') { return [] }
+        throw err
+      })
+      .then(existing => {
+        return BB.all(
+          existing.filter(f => f.match(/^@/))
+            .map(f => {
+              return readdirAsync(path.join(this.archiveDir, f))
+                .then(subfiles => subfiles.map(subf => `${f}/${subf}`))
+            })
+        )
+          .then(scoped => scoped.reduce((acc, scoped) => {
+            return acc.concat(scoped)
+          }, existing.filter(f => !f.match(/^@/))))
+      })
+      .then(allExisting => { this.existingArchives = new Set(allExisting) })
   }
 
   archiveTarball (spec, dep) {
@@ -173,56 +173,56 @@ class MyPrecious {
     const algorithms = dep.integrity && Object.keys(ssri.parse(dep.integrity))
     this.archives.add(relpath)
     return mkdirp(path.dirname(pkgPath))
-    .then(() => {
-      if (alreadyExists) {
-        this.log.silly('archiveTarball', `archive for ${spec} already exists`)
-        return ssri.fromStream(fs.createReadStream(pkgPath), {algorithms})
-      }
-      return new BB((resolve, reject) => {
-        const tardata = pacote.tarball.stream(spec, this.config.toPacote({
-          log: this.log,
-          resolved: dep.resolved &&
+      .then(() => {
+        if (alreadyExists) {
+          this.log.silly('archiveTarball', `archive for ${spec} already exists`)
+          return ssri.fromStream(fs.createReadStream(pkgPath), {algorithms})
+        }
+        return new BB((resolve, reject) => {
+          const tardata = pacote.tarball.stream(spec, this.config.toPacote({
+            log: this.log,
+            resolved: dep.resolved &&
           !dep.resolved.startsWith('file:') &&
           dep.resolved,
-          integrity: dep.integrity
-        }))
-        const gunzip = zlib.createGunzip()
-        const sriStream = ssri.integrityStream({algorithms})
-        const out = fs.createWriteStream(pkgPath)
-        let integrity
-        sriStream.on('integrity', i => { integrity = i })
-        tardata.on('error', reject)
-        gunzip.on('error', reject)
-        sriStream.on('error', reject)
-        out.on('error', reject)
-        out.on('close', () => resolve(integrity))
-        tardata
-        .pipe(gunzip)
-        .pipe(sriStream)
-        .pipe(out)
+            integrity: dep.integrity
+          }))
+          const gunzip = zlib.createGunzip()
+          const sriStream = ssri.integrityStream({algorithms})
+          const out = fs.createWriteStream(pkgPath)
+          let integrity
+          sriStream.on('integrity', i => { integrity = i })
+          tardata.on('error', reject)
+          gunzip.on('error', reject)
+          sriStream.on('error', reject)
+          out.on('error', reject)
+          out.on('close', () => resolve(integrity))
+          tardata
+            .pipe(gunzip)
+            .pipe(sriStream)
+            .pipe(out)
+        })
+          .tap(() => { this.pkgCount++ })
       })
-      .tap(() => { this.pkgCount++ })
-    })
-    .then(tarIntegrity => {
-      const resolvedPath = path.relative(this.prefix, pkgPath)
-      .replace(/\\/g, '/')
-      let integrity
-      if (!dep.integrity) {
-        integrity = tarIntegrity.toString()
-      } else if (dep.integrity.indexOf(tarIntegrity.toString()) !== -1) {
+      .then(tarIntegrity => {
+        const resolvedPath = path.relative(this.prefix, pkgPath)
+          .replace(/\\/g, '/')
+        let integrity
+        if (!dep.integrity) {
+          integrity = tarIntegrity.toString()
+        } else if (dep.integrity.indexOf(tarIntegrity.toString()) !== -1) {
         // TODO - this is a stopgap until `ssri#concat` (or a new
         // `ssri#merge`) become availble.
-        integrity = dep.integrity
-      } else {
+          integrity = dep.integrity
+        } else {
         // concat needs to be in this order 'cause apparently it's what npm
         // expects to do.
-        integrity = tarIntegrity.concat(dep.integrity).toString()
-      }
-      return {
-        resolved: `file:${resolvedPath}`,
-        integrity
-      }
-    })
+          integrity = tarIntegrity.concat(dep.integrity).toString()
+        }
+        return {
+          resolved: `file:${resolvedPath}`,
+          integrity
+        }
+      })
   }
 
   restoreDependencies (tree) {
@@ -245,11 +245,11 @@ class MyPrecious {
           ? dep.integrity
           : null
       }))
-      .then(mani => {
-        dep.resolved = mani._resolved || null
-        dep.integrity = mani._integrity || null
-      })
-      .then(next)
+        .then(mani => {
+          dep.resolved = mani._resolved || null
+          dep.integrity = mani._integrity || null
+        })
+        .then(next)
     })
   }
 
@@ -274,8 +274,8 @@ class MyPrecious {
     )) {
       const split = dep.integrity.split(/\s+/)
       const shortHash = ssri.parse(split[split.length - 1], {single: true})
-      .hexDigest()
-      .substr(0, 9)
+        .hexDigest()
+        .substr(0, 9)
       suffix += `-${shortHash}`
     }
     const filename = `${dep.name}-${suffix}.tar`
@@ -291,8 +291,8 @@ class MyPrecious {
         return next()
       } else {
         return this.archiveTarball(spec, dep)
-        .then(updated => Object.assign(dep, updated))
-        .then(() => next())
+          .then(updated => Object.assign(dep, updated))
+          .then(() => next())
       }
     }, {concurrency: 50, Promise: BB})
   }
@@ -343,15 +343,15 @@ class MyPrecious {
     })
     const lockPath = path.join(this.prefix, this.lockName)
     return readFileAsync(lockPath, 'utf8')
-    .then(file => {
-      const indent = detectIndent(file).indent || 2
-      const ending = detectNewline.graceful(file)
-      return writeFileAsync(
-        lockPath,
-        JSON.stringify(this.pkg._shrinkwrap, null, indent)
-        .replace(/\n/g, ending)
-      )
-    })
+      .then(file => {
+        const indent = detectIndent(file).indent || 2
+        const ending = detectNewline.graceful(file)
+        return writeFileAsync(
+          lockPath,
+          JSON.stringify(this.pkg._shrinkwrap, null, indent)
+            .replace(/\n/g, ending)
+        )
+      })
   }
 
   cleanupArchives () {
@@ -378,10 +378,10 @@ function stripBOM (str) {
 module.exports._readJson = readJson
 function readJson (jsonPath, name, ignoreMissing) {
   return readFileAsync(path.join(jsonPath, name), 'utf8')
-  .then(str => JSON.parse(stripBOM(str)))
-  .catch({code: 'ENOENT'}, err => {
-    if (!ignoreMissing) {
-      throw err
-    }
-  })
+    .then(str => JSON.parse(stripBOM(str)))
+    .catch({code: 'ENOENT'}, err => {
+      if (!ignoreMissing) {
+        throw err
+      }
+    })
 }
