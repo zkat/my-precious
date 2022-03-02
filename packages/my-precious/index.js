b066cb82c483ef3f24e635ad35f1a35943a4c77a
Aadil Siddiqui <9424117+aasiddiq@users.noreply.github.com>
2022-03-03 02:36:48 +0530

    Using correct library

--- packages/my-precious/index.js
+++ packages/my-precious/index.js
@@ -2,7 +2,7 @@
 
 'use strict'
 
-const MyPrecious = require('libprecious')
+const MyPrecious = require('libprecious-next')
 const npmlog = require('npmlog')
 const fromNpm = require('libprecious/lib/config/npm-config.js').fromNpm
 
@@ -13,39 +13,39 @@ if (require.main === module) {
 module.exports = cliMain
 function cliMain () {
   return fromNpm(process.argv)
-  .then(config => {
-    npmlog.level = config.get('loglevel')
-    const precious = new MyPrecious({
-      config,
-      log: npmlog
-    })
-    if (process.argv.find(x => x === 'reset')) {
-      return precious.unarchive()
-      .then(() => console.error('archive data removed'))
-    } else {
-      return precious.archive()
-      .then(details => {
-        const clauses = []
-        if (!details.pkgCount && !details.removed) {
-          clauses.push('done')
-        }
-        if (details.pkgCount) {
-          clauses.push(`archived ${details.pkgCount} package${
-            details.pkgCount === 1 ? '' : 's'
-          }`)
-        }
-        if (details.removed) {
-          clauses.push(`cleaned up ${details.pkgCount} archive${
-            details.removed === 1 ? '' : 's'
-          }`)
-        }
-        const time = details.runTime / 1000
-        console.error(`${clauses.join(' and ')} in ${time}s`)
+    .then(config => {
+      npmlog.level = config.get('loglevel')
+      const precious = new MyPrecious({
+        config,
+        log: npmlog
       })
-    }
-  })
-  .catch(err => {
-    npmlog.error('', err.message)
-    npmlog.verbose('', err.stack)
-  })
+      if (process.argv.find(x => x === 'reset')) {
+        return precious.unarchive()
+          .then(() => console.error('archive data removed'))
+      } else {
+        return precious.archive()
+          .then(details => {
+            const clauses = []
+            if (!details.pkgCount && !details.removed) {
+              clauses.push('done')
+            }
+            if (details.pkgCount) {
+              clauses.push(`archived ${details.pkgCount} package${
+                details.pkgCount === 1 ? '' : 's'
+              }`)
+            }
+            if (details.removed) {
+              clauses.push(`cleaned up ${details.pkgCount} archive${
+                details.removed === 1 ? '' : 's'
+              }`)
+            }
+            const time = details.runTime / 1000
+            console.error(`${clauses.join(' and ')} in ${time}s`)
+          })
+      }
+    })
+    .catch(err => {
+      npmlog.error('', err.message)
+      npmlog.verbose('', err.stack)
+    })
 }
